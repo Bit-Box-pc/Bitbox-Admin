b557f27a5857358ac0d64f828ff0b72dc35fd466
vishalp743 <vp0072003@gmail.com>
2024-08-05 18:14:37 +0530

    change in Warranty routes

--- routes/warrantyManagement.js
+++ routes/warrantyManagement.js
@@ -1161,8 +1161,9 @@ router.post('/update-warranty/:warrantyId', upload.single('billPdf'), async (req
             const warrantyImageSrc = getWarrantyImage(updatedWarrantyData.durationnew);
             const expiryDate = new Date(warranty.expiryDate).toDateString();
             console.log(warranty.serialNumber);
+            const xxx = warranty.serialNumber;
     
-            const serialDetails = await SerialNumber.findOne({ serialNumber: updatedWarrantyData.serialNumber });
+            const serialDetails = await SerialNumber.findOne({ serialNumber: xxx });
             console.log(serialDetails);
 
             const pdfContent = `
